896c7cff359dc4cd5e61fdc2e323f7e1c148386f
H M Mohidul Islam <mislam6789@gmail.com>
2016-12-08 01:21:11 +0600

    Road Design Improved

--- main.cpp
+++ main.cpp
@@ -29,6 +29,81 @@ void DrawMainRoad(){
     glVertex2i(800,220);
     glEnd();
 
+     ///====================================road divider
+    glBegin(GL_LINES);
+    glColor3f(1.0,1.0,1.0);
+    int dividerLine=0;
+    for(int i=0;i<20;i++){
+        glVertex2i(dividerLine+20,300);
+        glVertex2i(dividerLine+40,300);
+        dividerLine=dividerLine+40;
+    }
+    glEnd();
+
+    ///==================================== Bottom Road Side Bar
+
+
+    int changeX1=0,changeX2=40;
+
+    for(int i=0;i<20;i++){
+        glBegin(GL_POLYGON);
+        if(i%2==0){
+            glColor3ub(1,1,1);  //black color
+        }else{
+            glColor3ub(217, 217, 217);  //white color
+        }
+
+        glVertex2i(changeX1,180);
+        glVertex2i(changeX2,180);
+        glVertex2i(changeX2,210);
+        glVertex2i(changeX1,210);
+
+        changeX1=changeX2;
+        changeX2=changeX2+40;
+        glEnd();
+    }
+
+
+
+    ///==================================== Bottom Road Side Bar Top View
+    glBegin(GL_LINES);
+    glLineWidth(5);
+	glColor3f(204, 153, 0);     //yellow color
+    glVertex2i(0,210);
+    glVertex2i(800,210);
+    glEnd();
+
+     ///==================================== Top Road Side Bar
+
+
+    int TopChangeX1=0,TopchangeX2=40;
+
+    for(int i=0;i<20;i++){
+        glBegin(GL_POLYGON);
+        if(i%2==1){
+            glColor3ub(1,1,1);  //black color
+        }else{
+            glColor3ub(217, 217, 217);  //white color
+        }
+
+        glVertex2i(TopChangeX1,400);
+        glVertex2i(TopchangeX2,400);
+        glVertex2i(TopchangeX2,420);
+        glVertex2i(TopChangeX1,420);
+
+        TopChangeX1=TopchangeX2;
+        TopchangeX2=TopchangeX2+40;
+        glEnd();
+    }
+
+    ///==================================== Top Road Side Bar Top View
+    glBegin(GL_LINES);
+    glLineWidth(5);
+	glColor3f(204, 153, 0);     //yellow color
+    glVertex2i(0,420);
+    glVertex2i(800, 420);
+    glEnd();
+
 }
 
 void myDisplay(void)
@@ -41,10 +116,8 @@ void myDisplay(void)
 
 void myInit (void)
 {
-    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glClearColor(0.0,0.7,1.5,0.0);  //sky color
     glColor3f(0.0f, 0.0f, 0.0f);
-    //glPointSize(15.0);
-    //glLineWidth(4.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(0.0, 800.0, 0.0, 600.0);
@@ -56,7 +129,7 @@ int main(int argc, char** argv)
     glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize (800, 600);
     glutInitWindowPosition (0,0);
-    glutCreateWindow ("Hello OpenGL");
+    glutCreateWindow ("High Way Road View");
     glutDisplayFunc(myDisplay);
     myInit ();
     glutMainLoop();
